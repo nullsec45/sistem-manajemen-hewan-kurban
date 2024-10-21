6aabef5e5a43ed1bb512f5ab59d6c806bcf4189c
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-10-21 23:00:53 +0700

    feat/auth : create route for view auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get("/", function () {
     return view('welcome');
+})->name("/")->middleware('auth');
+
+
+Route::prefix("auth")->name("auth.")->controller(AuthController::class)->group(function(){
+    Route::get("login","login")->name("login");
+    Route::post("login","doLogin")->name("do-login");
+    Route::get("register","register")->name("register");
+    Route::get("forgot-password","forgotPassword")->name("forgot-password");
 });
