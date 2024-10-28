92420801f2d75870d3d204420b551686bb607594
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-10-28 23:12:07 +0700

    feat/auth : add route: auth.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ForgotPasswordController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
@@ -12,5 +13,10 @@ Route::prefix("auth")->name("auth.")->controller(AuthController::class)->group(f
     Route::get("login","login")->name("login");
     Route::post("login","doLogin")->name("do-login");
     Route::get("register","register")->name("register");
+    Route::post("register","doRegister")->name("do-register");
     Route::get("forgot-password","forgotPassword")->name("forgot-password");
+});
+
+Route::prefix("forgot-password")->name("forgot-password.")->controller(ForgotPasswordController::class)->group(function(){
+    Route::get("/","index")->name("index");
 });
