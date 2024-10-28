d11afab2dd9a712a74fec2f99c95aacd8b6ea1c9
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-10-28 23:11:43 +0700

    feat/auth : error message validation in view register.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,7 @@
 @extends('auth.index')
 
 @section('body')
+
 <div id="auth">  
     <div class="row h-100">
         <div class="col-lg-5 col-12">
@@ -11,37 +12,60 @@
                 <h1 class="auth-title">Sign Up</h1>
                 <p class="auth-subtitle mb-5">Input your data to register to our website.</p>
 
-                <form action="{{route('auth.register')}}">
+                <form action="{{route('auth.do-register')}}" method="POST">
                     @csrf
                     <div class="form-group position-relative has-icon-left mb-4">
-                        <input type="text" class="form-control form-control-xl" placeholder="Email">
+                        <input type="text" class="form-control form-control-xl" placeholder="Email" name="email">
                         <div class="form-control-icon">
                             <i class="bi bi-envelope"></i>
                         </div>
+                        @error("email")
+                            <div  class="text-danger">
+                                <p>{{$message}}</p>
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group position-relative has-icon-left mb-4">
-                        <input type="text" class="form-control form-control-xl" placeholder="Username">
+                        <input type="text" class="form-control form-control-xl" placeholder="Username" name="username">
                         <div class="form-control-icon">
                             <i class="bi bi-person"></i>
                         </div>
+                        @error("username")
+                            <div  class="invalid-feedback">
+                                {{$message}}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group position-relative has-icon-left mb-4">
-                        <input type="password" class="form-control form-control-xl" placeholder="Password">
+                        <input type="password" class="form-control form-control-xl" placeholder="Password" name="password">
                         <div class="form-control-icon">
                             <i class="bi bi-shield-lock"></i>
                         </div>
+                        @error("password")
+                            <div  class="text-danger">
+                                <p>{{$message}}</p>
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-group position-relative has-icon-left mb-4">
-                        <input type="password" class="form-control form-control-xl" placeholder="Confirm Password">
+                        <input type="password" class="form-control form-control-xl" placeholder="Confirm Password" name="confirm_password">
                         <div class="form-control-icon">
                             <i class="bi bi-shield-lock"></i>
                         </div>
+                        @error("confirm_password")
+                            <div class="text-danger">
+                                <p>{{$message}}</p>
+                            </div>
+                        @enderror
                     </div>
-                    <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Sign Up</button>
+                    <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5" type="submit">Sign Up</button>
                 </form>
                 <div class="text-center mt-5 text-lg fs-4">
-                    <p class='text-gray-600'>Already have an account? <a href="{{route('auth.login')}}" class="font-bold">Log
-                            in</a>.</p>
+                    <p class='text-gray-600'>Already have an account? 
+                        <a href="{{route('auth.login')}}" class="font-bold">
+                            Log in
+                        </a>.
+                    </p>
                 </div>
             </div>
         </div>
