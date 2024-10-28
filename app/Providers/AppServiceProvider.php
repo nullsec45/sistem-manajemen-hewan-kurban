00bec83c603d25d61c21bb973e9d6ecc29106806
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-10-28 23:10:48 +0700

    feat/auth : add password validation to app service provider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Validator::extend('letters', function ($attribute, $value, $parameters, $validator) {
+            return preg_match('/[a-zA-Z]/', $value);
+        });
+
+        Validator::extend('mixed', function ($attribute, $value, $parameters, $validator) {
+            return preg_match('/[a-z]/', $value) && preg_match('/[A-Z]/', $value);
+        });
+
+        Validator::extend('numbers', function ($attribute, $value, $parameters, $validator) {
+            return preg_match('/[0-9]/', $value);
+        });
+
+        Validator::extend('symbols', function ($attribute, $value, $parameters, $validator) {
+            return preg_match('/[!@#$%^&*(),.?":{}|<>]/', $value);
+        });
     }
 }
