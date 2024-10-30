68370a98c54952dd5c1897b62e2a0f86d113b4ff
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-10-30 22:20:44 +0700

    feat/dashboard : new templating view foot.

---
+++ resources/views/layouts/foot.blade.php
@@ -0,0 +1,9 @@
+<script src="{{asset('storage/assets/static/js/components/dark.js')}}"></script>
+<script src="{{asset('storage/assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
+<script src="{{asset('storage/assets/compiled/js/app.js')}}"></script>
+    
+<!-- Need: Apexcharts -->
+<script src="{{asset('storage/assets/extensions/apexcharts/apexcharts.min.js')}}"></script>
+<script src="{{asset('storage/assets/static/js/pages/dashboard.js')}}"></script>
+</body>
+</html>
