17bf50a9e710fceee824c0cc7039fb66a4d7724e
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-11-06 23:24:14 +0700

    feat/dashboard : templating dinamis sttle & script

--- resources/views/layouts/foot.blade.php
+++ resources/views/layouts/foot.blade.php
@@ -1,3 +1,4 @@
+@stack('prepend-script')
 <script src="{{asset('storage/assets/static/js/components/dark.js')}}"></script>
 <script src="{{asset('storage/assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
 <script src="{{asset('storage/assets/compiled/js/app.js')}}"></script>
@@ -5,5 +6,6 @@
 <!-- Need: Apexcharts -->
 <script src="{{asset('storage/assets/extensions/apexcharts/apexcharts.min.js')}}"></script>
 <script src="{{asset('storage/assets/static/js/pages/dashboard.js')}}"></script>
+@stack('addon-script')
 </body>
 </html>
