1033e3fe3a3bdc8a43fb764e6f19323a3773c162
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-11-16 22:57:06 +0700

    feat/jenis-hewan : new controller.

---
+++ app/Http/Controllers/JenisHewanQurbanController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JenisHewanQurbanController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function show(){
+
+    }
+
+    public function edit(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function destroy(){
+        
+    }
+}
